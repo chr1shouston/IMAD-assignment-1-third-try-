29e92a8509cbc8d33ea2d2888ba41af335e7a986
chr1shouston <st10536381@myemeris.edu.za>
2026-03-13 13:09:00 +0200

    Times of Day and If Statements
    Predefined times of day and made if statements for each

--- app/src/main/java/com/example/imadassignment1thirdtry/MainActivity.kt
+++ app/src/main/java/com/example/imadassignment1thirdtry/MainActivity.kt
@@ -29,6 +29,51 @@ class MainActivity : AppCompatActivity() {
         edtInput = findViewById(R.id.edtInput)
 
 
+        // Predefining the different sparks for each time of day
+        val morningSpark = "Send a 'Good Morning' text to a family member!"
+        val midmorningSpark = "Reach out to a colleague with a quick 'Thank you'!"
+        val afternoonSpark = "Share a funny Meme or interesting link with a friend!"
+        val afternoonSnackTimeSpark = "Send a quick 'Thinking of you' message!"
+        val dinnerSpark = "Call a friend or relative for a 5-minute catch-up!"
+        val nightSpark = "Leave a thoughtful comment on a friend's post"
+
+        // Waits for user to press the button, then executes code inside
+        btnCalculate.setOnClickListener {
+            // if the text the user inputs matches one of the desired times of day, then output the appropriate spark
+            if(edtInput.text.toString().lowercase() == "morning"){
+                tvResult.text = morningSpark
+            }
+            else if (edtInput.text.toString().lowercase() == "mid morning" ||
+                     edtInput.text.toString().lowercase() == "mid-morning"){
+
+                tvResult.text = midmorningSpark
+
+            }
+            else if (edtInput.text.toString().lowercase() == "afternoon"){
+                tvResult.text = afternoonSpark
+            }
+            else if (edtInput.text.toString().lowercase() == "snack time" ||
+                     edtInput.text.toString().lowercase() == "afternoon snack time"){
+
+                tvResult.text = afternoonSnackTimeSpark
+
+            }
+            else if (edtInput.text.toString().lowercase() == "dinner"){
+                tvResult.text = dinnerSpark
+            }
+            else if (edtInput.text.toString().lowercase() == "night" || edtInput.text.toString().lowercase() == "after dinner"){
+                tvResult.text = nightSpark
+            }
+
+            else {
+                tvResult.text = "Please enter one of the following: Morning, Mid-Morning, Afternoon, Afternoon Snack Time, Dinner, Night"
+            }
+
+        }
+
+        btnReset.setOnClickListener { tvResult.text = "" }
+
+
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
