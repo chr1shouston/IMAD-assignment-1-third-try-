5e83f88306261a8b76653fdb7e6fbaadb9e56c76
chr1shouston <st10536381@myemeris.edu.za>
2026-03-13 12:31:27 +0200

    declaring design elements as variables

--- app/src/main/java/com/example/imadassignment1thirdtry/MainActivity.kt
+++ app/src/main/java/com/example/imadassignment1thirdtry/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.imadassignment1thirdtry
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    // declaring design elements as variables
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+    private lateinit var btnReset: Button
+    private lateinit var edtInput: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
